681c40292248ba3f9d6f60b75f0e92f2a73bff9f
smit <smitkanani45@gmail.com>
2024-10-09 18:20:44 +0530

    first commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,22 @@
+import InstaIcon from "@/assets/icons/insta.svg";
+import XSocial from "@/assets/icons/x-social.svg";
+import TictokIcon from "@/assets/icons/tiktok.svg";
+import YoutubeIon from "@/assets/icons/youtube.svg";
+
 export const Footer = () => {
-  return null;
+  return (
+    <footer className="py-5 bg-black text-white/60 border-t border-white/20">
+      <div className="container">
+        <div className="flex flex-col sm:flex-row sm:justify-between">
+          <div className="text-center">© 2024 Your Company, Inc. All rights reserved</div>
+          <ul className="flex justify-center gap-2.5">
+            <li><XSocial /></li>
+            <li><InstaIcon /></li>
+            <li><TictokIcon /></li>
+            <li><YoutubeIon /></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 };
--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import acmeLogo from "../assets/images/acme.png";
 import quantumLogo from "../assets/images/quantum.png";
 import echoLogo from "../assets/images/echo.png";
 import celestialLogo from "../assets/images/celestial.png";
 import pulseLogo from "../assets/images/pulse.png";
 import apexLogo from "../assets/images/apex.png";
+import Image from "next/image";
+import { motion } from "framer-motion";
 
 const images = [
   { src: acmeLogo, alt: "Acme Logo" },
@@ -15,5 +19,42 @@ const images = [
 ];
 
 export const LogoTicker = () => {
-  return null;
+  return (
+    <div className="bg-black text-white py-[72px] sm:py-24">
+      <div className="container">
+        <h2 className="text-lg text-center text-white/70">
+          Trusted by the world's most innovative teams
+        </h2>
+        <div className="flex overflow-hidden mt-9 relative before:[''] before:z-10 before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 after:right-0 before:left-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))]">
+          <motion.div
+          transition={{
+            duration: 10,
+            ease: 'linear',
+            repeat: Infinity,
+          }}
+            initial={{ translateX: 0 }}
+            animate={{ translateX: '-50%'}}
+            className="flex gap-16 flex-none pr-16"
+          >
+            {images.map(({ src, alt }, index) => (
+              <Image
+                key={index}
+                src={src}
+                alt={alt}
+                className="flex-none h-8 w-auto"
+              />
+            ))}
+            {images.map(({ src, alt }, index) => (
+              <Image
+                key={index}
+                src={src}
+                alt={alt}
+                className="flex-none h-8 w-auto"
+              />
+            ))}
+          </motion.div>
+        </div>
+      </div>
+    </div>
+  );
 };
